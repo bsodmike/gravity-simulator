8ff5a382238eaa2950ca4e9a3b65a6afdaecaaef
Avee121 <142177694+Avee121@users.noreply.github.com>
2024-08-26 15:03:39 +0100

    fix boogs

--- src/fall.rs
+++ src/fall.rs
@@ -10,29 +10,31 @@ pub trait Falls {
         let mut result_force_x = 0.;
         let mut result_force_y = 0.;
 
+        let time_constant = ns_per_frame as f32 / 1e9;
+
         for other in left_pop.iter() {
-            let vals = self.compute_force_vec(other.as_ref(), ns_per_frame);
+            let vals = self.compute_force_vec(other.as_ref());
             result_force_x += vals[0];
             result_force_y += vals[1];
         }
 
         for other in right_pop.iter() {
-            let vals = self.compute_force_vec(other.as_ref(), ns_per_frame);
+            let vals = self.compute_force_vec(other.as_ref());
             result_force_x += vals[0];
             result_force_y += vals[1];
         }
 
-        let new_speed_x = self.get_x_vel() + (result_force_x / self.get_mass());
-        let new_speed_y = self.get_y_vel() + (result_force_y / self.get_mass());
+        let new_speed_x = start_vel_x + (result_force_x / self.get_mass()) * time_constant;
+        let new_speed_y = start_vel_y + (result_force_y / self.get_mass()) * time_constant;
 
         self.set_x_vel(new_speed_x);
         self.set_y_vel(new_speed_y);
 
-        self.set_x(self.get_x() + (start_vel_x + new_speed_x) / 2. * (ns_per_frame as f32 / 1e9));
-        self.set_y(self.get_y() + (start_vel_y + new_speed_y) / 2. * (ns_per_frame as f32 / 1e9));
+        self.set_x(self.get_x() + (start_vel_x + new_speed_x) / 2. * time_constant);
+        self.set_y(self.get_y() + (start_vel_y + new_speed_y) / 2. * time_constant);
     }
 
-    fn compute_force_vec(&mut self, other: &dyn Falls, ns_per_frame: u64) -> [f32; 2] {
+    fn compute_force_vec(&mut self, other: &dyn Falls) -> [f32; 2] {
         let dx = other.get_x() - self.get_x();
         let dy = other.get_y() - self.get_y();
         let dist = (dx * dx + dy * dy).sqrt();
@@ -41,10 +43,7 @@ pub trait Falls {
         //we have force and direction, use to modify x and y_vel
         let angle = dy.atan2(dx);
 
-        [
-            force * angle.cos() / self.get_mass(),
-            force * angle.sin() / self.get_mass(),
-        ]
+        [force * angle.cos(), force * angle.sin()]
     }
 
     fn get_x(&self) -> f32;
